fca3b4ddcae4d816348d63287f05b80012a28edd
dymjin <dymjin@gmail.com>
2026-04-14 11:05:09 +0200

    Remove unnecassary log

--- capitalize.test.js
+++ capitalize.test.js
@@ -1,9 +1,6 @@
 import { capitalize } from "./capitalize.js";
-console.log(capitalize("test"))
 
 test("capitalizes the word test", () => {
   expect(capitalize("test")).toBe("Test");
-  expect(capitalize("draw")).toBe("Draw")
+  expect(capitalize("draw")).toBe("Draw");
 });
-
-
