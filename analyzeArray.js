a18cae00541d74633d22b6a157a8f518d3366576
dymjin <dymjin@gmail.com>
2026-04-14 16:42:54 +0200

    Add analyzeArray function

---
+++ analyzeArray.js
@@ -0,0 +1,25 @@
+// https://stackoverflow.com/a/1527820
+function getRandomArbitrary(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+function makeRandomIntArr() {
+  let randomArrLen = Math.floor(getRandomArbitrary(5, 10));
+  let arr = Array.from({ length: randomArrLen }, () =>
+    Math.floor(Math.random() * 10),
+  );
+  return arr;
+}
+
+function analyzeArray(arr) {
+  let sortedArr = arr.sort((a, b) => a - b)[0];
+  let min = arr[0];
+  let max = arr[arr.length - 1];
+  let average = arr.reduce((prev, curr) => prev + curr) / arr.length;
+  let length = arr.length;
+
+  return { min, max, average, length };
+}
+
+let object = analyzeArray(makeRandomIntArr());
+export { object, analyzeArray };
